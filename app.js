908895d4175e6a387126c262241de358bf43502f
Kashmira Pardeshi <pardeshikashmira@gmail.com>
2018-06-17 13:59:01 -0400

    viewing and hiding comments

--- app.js
+++ app.js
@@ -87,15 +87,37 @@ function display_data(){
   for(var i = 0; i<user_array.length; i++){
     for(var j = 0; j<post_array.length; j++){
       if(user_array[i].id == post_array[j].userId){
-        html += `<p id = "user_name_${i}">Name: ${user_array[i].name}</p>
-                 <p id = "user_post_${i}">Title: ${post_array[i].title}</p>
-                 <p id = "user_post_${i}">Description: ${post_array[i].body}</p>
-                 <button id = "like_btn_${i}">Liked</button>
-                 <button id = "comment_btn_${i}">Comment</button>
-                 <button id = "delete_btn_${i}">Delete</button>`
+        html += `<div id = "post_${post_array[j].id}">
+                 <p id = "user_name_${i}"><strong>Name:</strong> ${user_array[i].name}</p>
+                 <p id = "user_post_${i}"><strong>Title: </strong> ${post_array[i].title}</p>
+                 <p id = "user_post_${i}"><strong>Description:</strong> ${post_array[i].body}</p>
+                 <button id = "like_btn_${j}">Liked</button>
+                 <button id = "comment_btn_${j}">Comment</button>
+                 <button id = "delete_btn_${j}">Delete</button>
+                 </div>`
        }
     }
   }
   jQuery('#display').html(html);
 }
 
+jQuery(document).on('click', 'button[id^="comment_btn_"]', function(){
+  commentHtml = "";
+  var tr_id = jQuery(this).attr('id').replace('comment_btn_', 'post_');
+  var trr = jQuery(this).attr('id').replace('comment_btn_', '');
+  var index_val = Number(trr);
+  var tempId = tr_id.replace('post_', 'temp_');
+  var err = jQuery('#'+ tempId).attr('id');
+  if(err){
+    $(".remove_comment").remove();
+  }
+  else{
+    for(var i = 1; i<comment_array.length; i++){
+      if(post_array[index_val].id == comment_array[i].postId){
+        commentHtml += `<div class = "remove_comment" id = "temp_${index_val}"><p>Comment: ${comment_array[i].body}</p></div>`
+      }
+    }
+  }
+  
+   jQuery('#delete_btn_'+index_val).after(commentHtml); 
+});
